73090e77b01fb2f962754c77c4886b79b99c5e91
Nirjhor Nath <nirjhor.nath@gmail.com>
2021-12-28 23:37:35 -0500

    eslint exclude type defs

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
-  interface ProcessEnv {
-    DATABASE_URL: string;
-    JWT_SECRET: string;
-  }
+    interface ProcessEnv {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    }
 }
