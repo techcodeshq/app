0ee675d3e23e3e7f941dbff2e6d325ccebbe82a7
Dashiell Elliott <dashiell.elliott@yahoo.com>
2022-01-07 22:16:43 -0500

    Add granting links to UI

--- src/components/event/link-row.tsx
+++ src/components/event/link-row.tsx
@@ -2,15 +2,30 @@ import { ExternalLinkIcon } from "@chakra-ui/icons";
 import {
   Button,
   chakra,
+  Divider,
+  Grid,
   GridItem,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
   Text,
   useBreakpointValue,
+  useColorModeValue,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { useMutation } from "@hooks/useMutation";
-import { EventLink } from "@typings";
+import { useQuery } from "@hooks/useQuery";
+import { EventLink, User } from "@typings";
 import Link from "next/link";
+import React from "react";
 import { useEvent } from "./context";
 import { LinkWithMetadata } from "./links-grid";
+import { MemberGrantRow } from "./member-row";
+import { GiPayMoney } from "react-icons/gi";
 
 export const LinksRow: React.FC<{
   link: LinkWithMetadata;
@@ -22,6 +37,10 @@ export const LinksRow: React.FC<{
     "patch",
     `/links/${event.id}`
   );
+  const bgColor = useColorModeValue("bg.100", "bg.800");
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { data } = useQuery<User[]>("/users");
 
   return (
     <>
@@ -45,13 +64,10 @@ export const LinksRow: React.FC<{
           fontWeight="500"
           borderRadius="20px"
           onClick={async () => {
-            console.log(link.id);
             const data = await toggle({
               id: link.id,
               value: !link.enabled,
             });
-
-            console.log(data);
           }}
           cursor="pointer"
           _hover={{
@@ -71,6 +87,49 @@ export const LinksRow: React.FC<{
           />
         </Link>
       </GridItem>
+      <GridItem>
+        <IconButton
+          onClick={onOpen}
+          aria-label="apply-manual"
+          icon={<GiPayMoney />}
+        />
+      </GridItem>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size="6xl"
+        scrollBehavior="inside"
+      >
+        <ModalOverlay />
+        <ModalContent bgColor={bgColor}>
+          <ModalHeader>Grant Link</ModalHeader>
+          <ModalBody>
+            <Grid
+              templateColumns={mobileGrid ? "repeat(3, 1fr)" : "repeat(5, 1fr)"}
+              gap="2rem"
+              padding="1.5rem"
+              fontWeight="bold"
+            >
+              {!mobileGrid && <GridItem>Avatar</GridItem>}
+              <GridItem>OSIS</GridItem>
+              <GridItem>Name</GridItem>
+              {!mobileGrid && <GridItem>Email Address</GridItem>}
+              <GridItem>Grant</GridItem>
+              {data &&
+                data.map((user) => (
+                  <React.Fragment key={user.id}>
+                    <MemberGrantRow user={user} link={link as EventLink} />
+                    <GridItem colSpan={mobileGrid ? 3 : 5}>
+                      <Divider />
+                    </GridItem>
+                  </React.Fragment>
+                ))}
+            </Grid>
+          </ModalBody>
+          <ModalCloseButton />
+        </ModalContent>
+      </Modal>
     </>
   );
 };
