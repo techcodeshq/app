f8e548177ed4f8746f5d4686bdf128ac1ccbf3e1
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-01-07 23:35:53 -0500

    link page rework

--- src/components/event/executive/link-dashboard.tsx
+++ src/components/event/executive/link-dashboard.tsx
@@ -1,29 +1,28 @@
 import {
   Box,
+  Button,
+  Center,
   Divider,
   Flex,
   Grid,
   GridItem,
   Heading,
+  HStack,
+  Image,
   useBreakpointValue,
   useColorModeValue,
-  Image,
-  Center,
 } from "@chakra-ui/react";
-import { MemberRow } from "@components/dashboard/executive/members-tab/member-row";
 import {
   HorizontalSidebar,
   VerticalSidebar,
 } from "@components/nav/base-sidebar";
+import { useMutation } from "@hooks/useMutation";
 import { useQuery } from "@hooks/useQuery";
-import {
-  EventLink,
-  EventLinkRedeem,
-  EventLinkRedeemStatus,
-  LinkApplyInstructions,
-} from "@typings";
-import React from "react";
+import { EventLink, EventLinkRedeem, LinkApplyInstructions } from "@typings";
+import { actionBasedValue } from "@lib/util/actionBasedValue";
 import QRCode from "qrcode.react";
+import React from "react";
+import Link from "next/link";
 
 interface LinkPageProps {
   link: EventLink & { metadata: LinkApplyInstructions[] };
@@ -47,19 +46,48 @@ export const LinkDashboard: React.FC<LinkPageProps> = ({ link, fullUrl }) => {
   const { data } = useQuery<Response>(`/links/redeemed/${link.id}`, {
     refreshInterval: 1000,
   });
+  const toggle = useMutation<EventLink, { id: String; value: boolean }>(
+    "/links",
+    "patch",
+    `/links/code/${link.code}`
+  );
+  const bgColor = useColorModeValue("bg.100", "bg.800");
+  const qrSize = useBreakpointValue({ base: 200, md: 225, lg: 275 });
 
   return (
     <Flex flexDirection={{ base: "column", md: "row" }} h="100vh">
       {isMobile ? <HorizontalSidebar /> : <VerticalSidebar />}
       <Flex w="100%" flexDir="column" flex="1">
-        <Heading p="2rem 2rem 0">
-          <Box as="span" fontWeight="650">
-            Link:
-          </Box>{" "}
-          <Box as="span" fontWeight="500">
-            {link.name}
-          </Box>
-        </Heading>
+        <Flex
+          alignItems="center"
+          p="2rem 2rem 0"
+          justifyContent="space-between"
+        >
+          <Heading>
+            <Box as="span" fontWeight="650">
+              Link:
+            </Box>{" "}
+            <Box as="span" fontWeight="500">
+              {link.name}
+            </Box>
+          </Heading>
+          <Button
+            onClick={async () => {
+              await toggle({
+                id: link.id,
+                value: !link.enabled,
+              });
+            }}
+            bgColor={link.enabled ? "green.300" : "red.300"}
+            _hover={{
+              bgColor: link.enabled ? "green.400" : "red.500",
+              transition: "background-color ease-in 200ms",
+            }}
+            color={bgColor}
+          >
+            {link.enabled ? "Disable" : "Enable"}
+          </Button>
+        </Flex>
         <Flex
           p="2rem"
           gap="2rem"
@@ -68,32 +96,42 @@ export const LinkDashboard: React.FC<LinkPageProps> = ({ link, fullUrl }) => {
           overflow="auto"
           flexDirection={{ base: "column", md: "row" }}
         >
+          {isMobile && (
+            <HStack>
+              <Link href="#redeemed">Redeemed</Link>
+              <Link href="#qr">QR Code</Link>
+              <Link href="#actions">Actions</Link>
+            </HStack>
+          )}
           <Flex flex="2">
             <Box
               bgColor={boxColor}
               borderRadius="0.4rem"
               width={{ base: "100%", md: null }}
               overflow="auto"
+              id="redeemed"
             >
               <Heading p="1.5rem 1.5rem 0 1.5rem" fontSize="1.5rem">
                 Redeemed
               </Heading>
               <Grid templateColumns="repeat(4, 1fr)" p="1.5rem" gap="2rem">
-                <GridItem fontWeight="600">Avatar</GridItem>
+                {!isMobile && <GridItem fontWeight="600">Avatar</GridItem>}
                 <GridItem fontWeight="600">Name</GridItem>
                 <GridItem fontWeight="600">Status</GridItem>
-                <GridItem fontWeight="600">Redeemed On</GridItem>
+                <GridItem fontWeight="600">Time</GridItem>
                 {data &&
                   data.map((item) => (
                     <>
-                      <GridItem alignSelf="center">
-                        <Image
-                          width="3rem"
-                          borderRadius="50%"
-                          alt={`${item.user.name}-avatar`}
-                          src={item.user.image}
-                        />
-                      </GridItem>
+                      {!isMobile && (
+                        <GridItem alignSelf="center">
+                          <Image
+                            width="3rem"
+                            borderRadius="50%"
+                            alt={`${item.user.name}-avatar`}
+                            src={item.user.image}
+                          />
+                        </GridItem>
+                      )}
                       <GridItem alignSelf="center">{item.user.name}</GridItem>
                       <GridItem alignSelf="center">
                         {item.statusDescription}
@@ -116,16 +154,15 @@ export const LinkDashboard: React.FC<LinkPageProps> = ({ link, fullUrl }) => {
               borderRadius="0.4rem"
               overflow="auto"
               width={{ base: "100%", md: null }}
+              id="qr"
             >
-              <Heading p="1.5rem 1.5rem 0 1.5rem" fontSize="1.5rem">
+              <Heading p="1.5rem" fontSize="1.5rem">
                 QRCode
               </Heading>
               <Center>
                 <QRCode
                   value={fullUrl || window?.location?.href}
-                  size={300}
-                  bgColor="#0C121A"
-                  fgColor="white"
+                  size={qrSize}
                 />
               </Center>
             </Flex>
@@ -136,51 +173,37 @@ export const LinkDashboard: React.FC<LinkPageProps> = ({ link, fullUrl }) => {
               borderRadius="0.4rem"
               overflow="auto"
               width={{ base: "100%", md: null }}
+              id="actions"
             >
               <Heading p="1.5rem 1.5rem 0 1.5rem" fontSize="1.5rem">
                 Actions
               </Heading>
-              <Grid templateColumns="repeat(3, 1fr)" p="1.5rem" gap="2rem">
+              <Grid templateColumns="repeat(2, 1fr)" p="1.5rem" gap="2rem">
                 <GridItem>Key</GridItem>
-                <GridItem>Action</GridItem>
                 <GridItem>Value</GridItem>
                 {link.metadata &&
                   link.metadata.map((md) => (
                     <>
                       <GridItem
-                        color={
-                          md.action === "INCREMENT"
-                            ? "green.300"
-                            : md.action === "DECREMENT"
-                            ? "red.300"
-                            : "white"
-                        }
+                        color={actionBasedValue(md.action, [
+                          "green.300",
+                          "red.400",
+                          null,
+                        ])}
                       >
                         {md.key}
                       </GridItem>
                       <GridItem
-                        color={
-                          md.action === "INCREMENT"
-                            ? "green.300"
-                            : md.action === "DECREMENT"
-                            ? "red.300"
-                            : "white"
-                        }
+                        color={actionBasedValue(md.action, [
+                          "green.300",
+                          "red.400",
+                          null,
+                        ])}
                       >
-                        {md.action}
-                      </GridItem>
-                      <GridItem
-                        color={
-                          md.action === "INCREMENT"
-                            ? "green.300"
-                            : md.action === "DECREMENT"
-                            ? "red.300"
-                            : "white"
-                        }
-                      >
+                        {actionBasedValue(md.action, ["+", "-", "="])}
                         {md.value}
                       </GridItem>
-                      <GridItem colSpan={3}>
+                      <GridItem colSpan={2}>
                         <Divider />
                       </GridItem>
                     </>
