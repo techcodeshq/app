b40b5f1330399d4ce6e99fdc8d64c3d6a89b6cd4
SantiagoVira <8bitharrypotter@gmail.com>
2022-01-12 22:31:37 -0500

    Landing page look like slightly less shit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,28 @@
-import { Box, Button, Link } from "@chakra-ui/react";
+import { Center, Button, Link } from "@chakra-ui/react";
 import { withOsisRedirect } from "@lib/util/osisRedirect";
 import { signIn, signOut } from "next-auth/react";
 
 const Index = ({ session }) => {
-  return (
-    <Box>
-      <Button
-        onClick={() =>
-          !session
-            ? signIn("google", {
-                callbackUrl: `/auth/register?${new URLSearchParams({
-                  url: window.location.href,
-                })}`,
-              })
-            : signOut()
-        }
-      >
-        {session ? "Sign Out" : "Sign In"}
-      </Button>
-      {session && <Link href="/dashboard">Dashboard</Link>}
-    </Box>
-  );
+    return (
+        <Center height="66vh">
+            <Button
+                onClick={() =>
+                    !session
+                        ? signIn("google", {
+                              callbackUrl: `/auth/register?${new URLSearchParams(
+                                  {
+                                      url: window.location.href,
+                                  }
+                              )}`,
+                          })
+                        : signOut()
+                }
+            >
+                {session ? "Sign Out" : "Sign In"}
+            </Button>
+            {session && <Link href="/dashboard">Dashboard</Link>}
+        </Center>
+    );
 };
 
 export const getServerSideProps = withOsisRedirect();
