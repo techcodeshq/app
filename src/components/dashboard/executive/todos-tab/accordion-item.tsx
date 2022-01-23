87f4d28e5c88461b961b35a7f66c89826d50d4a9
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-01-22 23:28:11 -0500

    todo page nicer

--- src/components/dashboard/executive/todos-tab/accordion-item.tsx
+++ src/components/dashboard/executive/todos-tab/accordion-item.tsx
@@ -8,6 +8,7 @@ import {
   AccordionPanel,
   Divider,
   Box,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { EventTask } from "@typings";
 import moment from "moment";
@@ -18,36 +19,43 @@ export const TodoAccordion: React.FC<{
   title: string;
   data: EventTask[];
   filter: (task: EventTask) => boolean;
-}> = ({ title, data, filter }) => (
-  <AccordionItem border="none" mb="0.5rem">
-    <AccordionButton bgColor="bg.800" shadow="md" _hover={{}} p="1rem">
-      <Flex alignItems="center" justifyContent="space-between" w="100%">
-        <Heading fontWeight="500" fontSize="2rem">
-          {title}
-        </Heading>
-        <AccordionIcon />
-      </Flex>
-    </AccordionButton>
-    <AccordionPanel pb={4}>
-      {data &&
-        data.filter(filter).map((task) => (
-          <Box
-            key={task.id}
-            transition="transform 0.2ms ease-in"
-            _hover={{ transform: "translateY(0.1rem)", cursor: "pointer" }}
-          >
-            <Flex
-              alignItems="center"
-              justifyContent="space-between"
-              fontSize="1.5rem"
-              p="1rem 0"
-            >
-              <Text>{task.name}</Text>
-              <Text>{moment(task.dueDate).calendar()}</Text>
-            </Flex>
-            <Divider bgColor="accent.600" />
-          </Box>
-        ))}
-    </AccordionPanel>
-  </AccordionItem>
-);
+}> = ({ title, data, filter }) => {
+  const bgColor = useColorModeValue("bg.100", "bg.800");
+
+  return (
+    <AccordionItem border="none" mb="0.5rem">
+      <AccordionButton bgColor={bgColor} shadow="sm" _hover={{}} p="1rem">
+        <Flex alignItems="center" justifyContent="space-between" w="100%">
+          <Heading fontWeight="500" fontSize="2rem">
+            {title}
+          </Heading>
+          <AccordionIcon />
+        </Flex>
+      </AccordionButton>
+      <AccordionPanel pb={4}>
+        {data &&
+          data
+            .filter((task) => !task.completedAt)
+            .filter(filter)
+            .map((task) => (
+              <Box
+                key={task.id}
+                transition="transform 0.2ms ease-in"
+                _hover={{ transform: "translateY(0.1rem)", cursor: "pointer" }}
+              >
+                <Flex
+                  alignItems="center"
+                  justifyContent="space-between"
+                  fontSize="1.5rem"
+                  p="1rem 0"
+                >
+                  <Text>{task.name}</Text>
+                  <Text>{moment(task.dueDate).calendar()}</Text>
+                </Flex>
+                <Divider bgColor="accent.600" />
+              </Box>
+            ))}
+      </AccordionPanel>
+    </AccordionItem>
+  );
+};
