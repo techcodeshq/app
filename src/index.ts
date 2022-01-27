913c44b2fa6e7d943ad3f46529611421e57b380d
Dashiell Elliott <dashiell.elliott@yahoo.com>
2022-01-27 16:25:20 -0500

    start websockets things

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import { routes } from "./routes";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { wsAuthenticated } from "./middlewares/authenticated";
 
 export const app = express();
 
@@ -17,6 +20,15 @@ app.use(
 app.use(cookieParser());
 app.use(routes);
 
-app.listen(8000, () => {
+const server = createServer(app);
+const io = new Server(server);
+
+io.use(wsAuthenticated);
+
+io.on("connection", (socket) => {
+    console.log("A user connected", socket);
+});
+
+server.listen(8000, () => {
     console.log("Server Started on Port: 8000");
 });
