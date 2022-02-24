e4af2328865648bf500a53693c6ab3af107acd9e
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-02-24 15:52:43 -0500

    progress bar hook

--- packages/artemis/src/pages/_app.tsx
+++ packages/artemis/src/pages/_app.tsx
@@ -2,41 +2,20 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AnimatePresence, motion } from "framer-motion";
 import { SessionProvider } from "next-auth/react";
 import { SWRConfig } from "swr";
-import NProgress from "nprogress";
 import theme from "../theme";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import "../../public/styles/nprogress.css";
 import "github-markdown-css";
 import "swiper/css";
 import "react-mde/lib/styles/css/react-mde-all.css";
+import { useProgressBar } from "@hooks/useProgressBar";
 
 function MyApp({ Component, pageProps }) {
+  useProgressBar();
   const variants = {
     hidden: { opacity: 0 },
     enter: { opacity: 1 },
     exit: { opacity: 0 },
   };
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleStart = (url) => {
-      NProgress.start();
-    };
-    const handleStop = () => {
-      NProgress.done();
-    };
-
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleStop);
-    router.events.on("routeChangeError", handleStop);
-
-    return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleStop);
-      router.events.off("routeChangeError", handleStop);
-    };
-  }, [router]);
 
   return (
     <SWRConfig>
